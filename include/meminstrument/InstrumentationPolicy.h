de284b6afdc775117e2c13bab241e3700ab6bc3d
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2017-07-12 17:01:15 +0200

    Move preliminary code to WitnessStrategy

--- include/meminstrument/InstrumentationPolicy.h
+++ include/meminstrument/InstrumentationPolicy.h
@@ -27,9 +27,6 @@ public:
   virtual void classifyTargets(std::vector<ITarget> &Dest,
                                llvm::Instruction *Loc) = 0;
 
-  virtual void instrumentFunction(llvm::Function &Func,
-                                  std::vector<ITarget> &Targets) = 0;
-
   virtual ~InstrumentationPolicy() {}
 };
 
@@ -38,16 +35,11 @@ public:
   virtual void classifyTargets(std::vector<ITarget> &Dest,
                                llvm::Instruction *Loc) override;
 
-  virtual void instrumentFunction(llvm::Function &Func,
-                                  std::vector<ITarget> &Targets) override;
-
   BeforeOutflowPolicy(const llvm::DataLayout &DL) : DL(DL) {}
 
 private:
   const llvm::DataLayout &DL;
   size_t getPointerAccessSize(llvm::Value *V);
-
-  llvm::Value *createWitness(llvm::Value *Instrumentee);
 };
 
 } // end namespace meminstrument
---
+++ include/meminstrument/WitnessStrategy.h
@@ -0,0 +1,25 @@
+//===--- meminstrument/WitnessStrategy.h -- MemSafety Instr. --*- C++ -*---===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file TODO doku
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef MEMINSTRUMENT_WITNESSSTRATEGY_H
+#define MEMINSTRUMENT_WITNESSSTRATEGY_H
+
+#include "meminstrument/ITarget.h"
+
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Value.h"
+
+namespace meminstrument {} // end namespace meminstrument
+
+#endif // MEMINSTRUMENT_WITNESSSTRATEGY_H
