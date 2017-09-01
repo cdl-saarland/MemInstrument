2f8a40b80040ac39e512fa72834c0072a818af50
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2017-09-01 16:07:37 +0200

    Add and unxfail tests

---
+++ test/splay/almost_null_deref.c
@@ -0,0 +1,12 @@
+// RUN: %clang -emit-llvm -c -S -o %t0.ll %s
+// RUN: %opt %loadlibs -mem2reg -memsafety-genchecks %t0.ll -memsafety-imechanism=splay -S > %t1.ll
+// RUN: %clink -ldl -l:libsplay.a -o %t2 %t1.ll
+// RUN: %not %t2 2> %t3.log
+// RUN: fgrep "Memory safety violation!" %t3.log
+
+int main(void)
+{
+  int *a = (void*)0;
+  return a[5];
+}
+
---
+++ test/splay/null_argument_passing.c
@@ -0,0 +1,20 @@
+// RUN: %clang -emit-llvm -c -S -o %t0.ll %s
+// RUN: %opt %loadlibs -mem2reg -memsafety-genchecks %t0.ll -memsafety-imechanism=splay -S > %t1.ll
+// RUN: %clink -ldl -l:libsplay.a -o %t2 %t1.ll
+// RUN: %t2
+
+#define NULL ((void*)0)
+
+int foo(int *p) {
+  if (p == NULL) {
+    return 0;
+  } else {
+    return *p;
+  }
+}
+
+int main(void)
+{
+  return foo(NULL);
+}
+
