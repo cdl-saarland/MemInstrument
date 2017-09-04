4948226afd8d4b0357a35b341c80934b8ade985e
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2017-09-04 10:48:38 +0200

    Add testcases for arguments to the main function

---
+++ test/splay/main_arg_invalid1.c
@@ -0,0 +1,11 @@
+// RUN: %clang -emit-llvm -c -S -o %t0.ll %s
+// RUN: %opt %loadlibs -mem2reg -memsafety-genchecks %t0.ll -memsafety-imechanism=splay -S > %t1.ll
+// RUN: %clink -ldl -l:libsplay.a -o %t2 %t1.ll
+// RUN: %not %t2 foo0 2> %t3.err
+// RUN: fgrep "Memory safety violation!" %t3.err
+
+int main(int argc, char** argv)
+{
+  return argv[1][5] - '0';
+}
+
