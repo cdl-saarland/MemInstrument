ef938e28862b785fedc7db9498dfa3f95ab7ba03
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2017-09-19 16:49:57 +0200

    Add testcase with nested structs and doubles

---
+++ test/splay/double_struct.c
@@ -0,0 +1,24 @@
+// RUN: %clang -emit-llvm -c -S -o %t0.ll %s
+// RUN: %opt %loadlibs -mem2reg -memsafety-genchecks %t0.ll -memsafety-imechanism=splay -memsafety-splay-verbose -S > %t1.ll
+// RUN: %clink -ldl -l:libsplay.a -o %t2 %t1.ll
+// RUN: %t2
+
+#include <stdint.h>
+
+struct S {
+  struct T {
+    double x;
+    double y;
+  } t;
+  double z;
+};
+
+int main(void)
+{
+  struct S s;
+
+  double user_time = (double) s.t.x + s.t.y/1000000.0;
+
+  return 0;
+}
+
