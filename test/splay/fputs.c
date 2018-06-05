c2faf0548a5d5e652aeb00c2066a4c0ce1ba37fd
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2018-06-05 10:29:17 +0200

    Add debug flags for a test case

--- test/splay/fputs.c
+++ test/splay/fputs.c
@@ -1,6 +1,6 @@
-// RUN: %clang -emit-llvm -c -S -o %t0.ll %s
+// RUN: %clang -g -emit-llvm -c -S -o %t0.ll %s
 // RUN: %opt %loadlibs -mem2reg -meminstrument %t0.ll -mi-config=splay -mi-verbose -S > %t1.ll
-// RUN: %clink -ldl -l:libsplay.a -o %t2 %t1.ll
+// RUN: %clink -g -ldl -l:libsplay.a -o %t2 %t1.ll
 // RUN: %t2
 
 #include <stdio.h>
