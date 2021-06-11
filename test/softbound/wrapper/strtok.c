061625c879c4f1b24a910f0b1ef3eb381664e76b
Tina Jung <t.jung@cs.uni-saarland.de>
2021-06-11 16:08:21 +0200

    Add strtok wrapper tests for metadata propagation

---
+++ test/softbound/wrapper/strtok.c
@@ -0,0 +1,28 @@
+// RUN: %clang -Xclang -load -Xclang %passlib -O1 %s -mllvm -mi-config=softbound -emit-llvm -S -o %t0.ll
+// RUN: %clang %t0.ll %linksb -o %t
+// RUN: %t "hallo::world"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int printByteWise(char *str) {
+
+    while (*str != '\0') {
+        printf("%c", *str);
+        str++;
+    }
+    printf("\n");
+    return *str;
+}
+
+int main(int argc, char *argv[]) {
+
+    char *subStr = strtok(argv[1], ":");
+    while (subStr) {
+        printByteWise(subStr);
+        subStr = strtok(NULL, ":");
+    }
+
+    return 0;
+}
