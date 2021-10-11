6ec8ccd0a3716738fc5be14484633c4f321fedce
Tina Jung <t.jung@cs.uni-saarland.de>
2021-10-11 14:47:22 +0200

    Add test for the gmtime softbound wrapper

---
+++ test/softbound/wrapper/gmtime.c
@@ -0,0 +1,12 @@
+// RUN: %clang -Xclang -load -Xclang %passlib -O1 %s -mllvm -mi-config=softbound %linksb -o %t
+// RUN: %t
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void) {
+    long someNum = 2;
+    struct tm *sinceTime = gmtime(&someNum);
+    printf("Some infos: %d %d\n", sinceTime->tm_sec, sinceTime->tm_isdst);
+    return 0;
+}
