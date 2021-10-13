3e20a3ab5b90d0c4af876b8741d72ff3def73818
Tina Jung <t.jung@cs.uni-saarland.de>
2021-10-13 11:50:54 +0200

    Setup lowfat testing

---
+++ test/lowfat/aggregate_contains_ptr_invalid.c
@@ -0,0 +1,27 @@
+// RUN: %clang -Xclang -load -Xclang %passlib -O1 %s -mllvm -mi-config=lowfat %linklowfat -o %t
+// RUN: %not --crash %t 2> /dev/null
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int x;
+    int *ptr;
+} InfoWithPtr;
+
+
+InfoWithPtr f();
+
+int main(int argc, char const *argv[]) {
+    InfoWithPtr thing = f();
+
+    printf("%i %i\n", *(thing.ptr+17), thing.x);
+    return 0;
+}
+
+InfoWithPtr f() {
+    InfoWithPtr thing;
+    thing.x = 5;
+    thing.ptr = malloc(1);
+    return thing;
+}
