e29711264ec9508cc862f3db9d97aa5245d925be
Tina Jung <t.jung@cs.uni-saarland.de>
2022-04-26 15:21:00 +0200

    Remove unused debug output which is not available in a release build, add build requirement where necessary

--- test/lowfat/globals/char_array_one_past_end_ptr_deref.c
+++ test/lowfat/globals/char_array_one_past_end_ptr_deref.c
@@ -1,4 +1,4 @@
-// RUN: %clang -mllvm -debug-only=meminstrument -Xclang -load -Xclang %passlib -mcmodel=large -O1 %s -mllvm -mi-config=lowfat -emit-llvm -S -o %t.ll
+// RUN: %clang -Xclang -load -Xclang %passlib -mcmodel=large -O1 %s -mllvm -mi-config=lowfat -emit-llvm -S -o %t.ll
 // RUN: %clang -mcmodel=large %t.ll %linklowfat -o %t
 // RUN: %not --crash %t 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2>&1 | %filecheck %s
 
